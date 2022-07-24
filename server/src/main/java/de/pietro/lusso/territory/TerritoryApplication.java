dbd9494656cb25e1d63dd1d1107afdd46fd5d783
hydrogen2oxygen <luxuspeter@googlemail.com>
2022-07-24 12:13:19 +0200

    Security.setProperty("crypto.policy", "unlimited");

--- server/src/main/java/de/pietro/lusso/territory/TerritoryApplication.java
+++ server/src/main/java/de/pietro/lusso/territory/TerritoryApplication.java
@@ -3,10 +3,13 @@ package de.pietro.lusso.territory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Security;
+
 @SpringBootApplication
 public class TerritoryApplication {
 
 	public static void main(String[] args) {
+		Security.setProperty("crypto.policy", "unlimited");
 		SpringApplication.run(TerritoryApplication.class, args);
 	}
 }
